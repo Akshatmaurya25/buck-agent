9e2da6c8bd5b2b506f823b870b9a7b1af0c9245a
Pranesh <pranesh25joshi@gmail.com>
2025-02-07 00:21:34 +0530

    Add initial files and components for AI chat interface

---
+++ ai-chat-interface/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+// import Sofa from "@/sofa";
+import Page from "../page"
+
+export default function SyntheticV0PageForDeployment() {
+  return (
+    <div>
+      <Page />
+      {/* <Sofa /> */}
+    </div>
+)
+}
