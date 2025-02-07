ab05efbad58187012ae13eef2380e838dd148346
Akshat <akshatmaurya25@gmail.com>
2025-02-07 20:24:53 +0530

    added new functions

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,7 @@
 import { GameWorker } from "@virtuals-protocol/game";
 import { helloFunction, searchTweetsFunction, replyToTweetFunction, postTweetFunction } from "./functions";
 import { buyCryptoFunction, getCryptoPriceFunction, getWalletBalanceFunction, sellCryptoFunction, transferCryptoFunction ,getGeneralBalanceFunction, getSeiWalletBalance, transferSEI } from "./cryptoFunctions";
+import { MindshareFunction, SearchTweet } from "./CookieSwarm/functions/Cookiefunctions";
 
 
 export const helloWorker = new GameWorker({
@@ -33,7 +34,17 @@ export const cryptoWorker = new GameWorker({
     id: "crypto_worker",
     name: "Crypto worker",
     description: "This is the worker that will be used to interact with the crypto market, the user will be able to buy and sell crypto. Also the user will be able to see the crypto market and the crypto prices. User can also see the crypto news and the crypto trends. It is also able to transfer crypto to other wallets using wallet address",
-    functions: [getCryptoPriceFunction,getSeiWalletBalance, transferSEI ,buyCryptoFunction, sellCryptoFunction, transferCryptoFunction, getWalletBalanceFunction, getGeneralBalanceFunction],
+    functions: [
+       SearchTweet, 
+        MindshareFunction,
+        getCryptoPriceFunction,
+        getSeiWalletBalance,
+         transferSEI ,
+         buyCryptoFunction,
+          sellCryptoFunction,
+           transferCryptoFunction,
+            getWalletBalanceFunction,
+             getGeneralBalanceFunction],
     // Optional: Provide environment to LLP
     // getEnvironment: async () => {
 
